308a1cd335dd2479951a0e6f8172727fc5b93a0d
navneeet94 <98764448+navneeet94@users.noreply.github.com>
2023-08-07 23:39:02 +0530

    update code

--- app.py
+++ app.py
@@ -1,25 +1,15 @@
 from flask import Flask,jsonify
 from flask_cors import CORS
 import pandas as pd;
+import pickle as pkl;
 
-movie_df = pd.read_csv('./data/movie_id.csv')
-
+data = pkl.load(open('./data/movie_list.pkl','rb'))
 app = Flask(__name__)
 CORS(app)
 
-def extractKeyVal(value):
-    a,id,name= value
-    return {"id":str(id),"name":name}
-
-def movieList():
-    movie_list = []
-    for i in range(len(movie_df)):
-        movie_list.append(extractKeyVal(movie_df.iloc[i]))
-    return jsonify(movie_list)
-
 @app.route("/api/movie-ids")
 def home():
-    return movieList()
+    return jsonify(data)
 
 
 if __name__ == "__main__":
